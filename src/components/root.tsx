78dcfe01b0da716d5a9417ad80a7aa20a7fbba81
Dmitry Sokolovsky <assholedrummer665@yandex.ru>
2018-02-22 23:21:39 +0300

    add mobx/socket.io dependencies

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -4,10 +4,23 @@ import { Header } from './';
 import { Sidebar } from './sidebar/sidebar.component';
 
 import './root.scss';
+const url = 'http://localhost:3231';
+const io = require('socket.io-client');
 
 export class Root extends React.Component {
     constructor(props: {}) {
         super(props);
+    }
+    
+    componentDidMount() {
+        this.initSocket();
+    }
+
+    initSocket = () => {
+        const socket = io(url);
+        socket.on('connect', () => {
+            console.log('CONNECTED');
+        });
     }
 
     public render(): JSX.Element {
